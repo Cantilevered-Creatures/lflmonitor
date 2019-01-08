df6c1dd4a886a7506492bf356211adb3ae88ed91
HoveringHalibut <rich@ericksons.us>
2019-01-07 21:11:21 -0600

    Add update script

---
+++ scripts/update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curCommit=$(curl --silent https://api.github.com/repos/HoveringHalibut/fllmonitor/git/refs/heads/master | jq -r '.object.sha')
+
+if [ "$curCommit" != "$([ -f ./lastCommit ] && cat ./lastCommit)" ]; then
+  git pull -q
+  echo $curCommit > lastCommit
+fi
