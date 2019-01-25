5eba1b16756963928e110f96d9dd5ab48fabe513
HoveringHalibut <rich@ericksons.us>
2019-01-25 00:10:30 -0600

    Switch to ADS1115 libraries

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import RPi.GPIO as GPIO
 
 import board
 import busio
-import adafruit_ads1x15.ads1015 as ADS
+import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
 import blinkt
@@ -28,7 +28,7 @@ app = Flask(__name__)
 i2c = busio.I2C(board.SCL, board.SDA)
 
 # Create the ADC object using the I2C bus
-ads = ADS.ADS1x15(i2c)
+ads = ADS.ADS1115(i2c)
 
 # Create single-ended input on channel 0
 chan = AnalogIn(ads, ADS.P0)
