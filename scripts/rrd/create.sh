b4f732a5906eefe6f6f80ad8da5b0aea1fed6456
HoveringHalibut <rich@ericksons.us>
2019-01-27 02:01:25 -0600

    Add --showtime to xport command to include timestamp

--- scripts/rrd/create.sh
+++ scripts/rrd/create.sh
@@ -8,9 +8,9 @@ fi
 
 rrdtool create $RRD_PATH \
    --start now-2h --step 1s \
-   DS:vbattery:GAUGE:5m:0:24000 \
-   DS:vpanel:GAUGE:5m:0:24000 \
-   RRA:AVERAGE:0.5:1s:10d \
+   DS:vbattery:GAUGE:5s:0:15 \
+   DS:vpanel:GAUGE:5s:0:15 \
+   RRA:AVERAGE:0.5:5s:10d \
    RRA:AVERAGE:0.5:1m:90d \
    RRA:AVERAGE:0.5:1h:18M \
    RRA:AVERAGE:0.5:1d:10y
--- scripts/rrd/export.sh
+++ scripts/rrd/export.sh
@@ -6,43 +6,43 @@ else
   source './app.cfg.example'
 fi
 
-rrdtool xport -s now-3h -e now --step 300 \
+rrdtool xport --showtime -s now-3h -e now --step 300 \
 DEF:a=$RRD_PATH:vbattery:AVERAGE \
 DEF:b=$RRD_PATH:vpanel:AVERAGE \
 XPORT:a:"Battery" \
 XPORT:b:"Panel" > $XML_PATH/voltage3h.xml
 
-rrdtool xport -s now-24h -e now --step 900 \
+rrdtool xport --showtime -s now-24h -e now --step 900 \
 DEF:a=$RRD_PATH:vbattery:AVERAGE \
 DEF:b=$RRD_PATH:vpanel:AVERAGE \
 XPORT:a:"Battery" \
 XPORT:b:"Panel" > $XML_PATH/voltage24h.xml
 
-rrdtool xport -s now-48h -e now --step 1800 \
+rrdtool xport --showtime -s now-48h -e now --step 1800 \
 DEF:a=$RRD_PATH:vbattery:AVERAGE \
 DEF:b=$RRD_PATH:vpanel:AVERAGE \
 XPORT:a:"Battery" \
 XPORT:b:"Panel" > $XML_PATH/voltage48h.xml
 
-rrdtool xport -s now-8d -e now --step 7200 \
+rrdtool xport --showtime -s now-8d -e now --step 7200 \
 DEF:a=$RRD_PATH:vbattery:AVERAGE \
 DEF:b=$RRD_PATH:vpanel:AVERAGE \
 XPORT:a:"Battery" \
 XPORT:b:"Panel" > $XML_PATH/voltage1w.xml
 
-rrdtool xport -s now-1month -e now --step 10800 \
+rrdtool xport --showtime -s now-1month -e now --step 10800 \
 DEF:a=$RRD_PATH:vbattery:AVERAGE \
 DEF:b=$RRD_PATH:vpanel:AVERAGE \
 XPORT:a:"Battery" \
 XPORT:b:"Panel" > $XML_PATH/voltage1m.xml
 
-rrdtool xport -s now-3month -e now --step 43200 \
+rrdtool xport --showtime -s now-3month -e now --step 43200 \
 DEF:a=$RRD_PATH:vbattery:AVERAGE \
 DEF:b=$RRD_PATH:vpanel:AVERAGE \
 XPORT:a:"Battery" \
 XPORT:b:"Panel" > $XML_PATH/voltage3m.xml
 
-rrdtool xport -s now-1y -e now --step 86400 \
+rrdtool xport --showtime -s now-1y -e now --step 86400 \
 DEF:a=$RRD_PATH:vbattery:AVERAGE \
 DEF:b=$RRD_PATH:vpanel:AVERAGE \
 XPORT:a:"Battery" \
