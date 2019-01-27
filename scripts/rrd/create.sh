f73a5e0c7a0b2286347fcd0cc886c869b4f3c19b
HoveringHalibut <rich@ericksons.us>
2019-01-27 09:20:57 -0600

    Rework rrd db settings

--- scripts/rrd/create.sh
+++ scripts/rrd/create.sh
@@ -7,9 +7,9 @@ else
 fi
 
 rrdtool create $RRD_PATH \
-   --start now-2h --step 1s \
-   DS:vbattery:GAUGE:5s:0:15 \
-   DS:vpanel:GAUGE:5s:0:15 \
+   --start now-2h --step 5s \
+   DS:vbattery:GAUGE:5m:0:50 \
+   DS:vpanel:GAUGE:5m:0:50 \
    RRA:AVERAGE:0.5:5s:10d \
    RRA:AVERAGE:0.5:1m:90d \
    RRA:AVERAGE:0.5:1h:18M \
