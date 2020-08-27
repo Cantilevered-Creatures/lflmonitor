fdcb51da9c36cc43a73293525ae462cdba660840
Rich Erickson <rich.erickson@fpx.com>
2020-08-26 21:47:10 -0500

    Fix javascript

--- static/music.js
+++ static/music.js
@@ -2,6 +2,6 @@ setInterval(function() {
   $.ajax({
     url: location.protocol + '//' + document.domain + ':' + location.port + '/currentsong'
   }).then(function(data){
-    $("#playingSong").val(data.name);
+    $("#playingSong").innerText = data.name;
   });
 }, 5000);
