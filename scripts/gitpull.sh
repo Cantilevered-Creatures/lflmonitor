4af1fd864b80be04c6897a69650d4f685a4aa64d
HoveringHalibut <rich@ericksons.us>
2020-09-12 21:22:51 -0500

    Add --recurse-submodules to git pull

--- scripts/gitpull.sh
+++ scripts/gitpull.sh
@@ -3,7 +3,7 @@
 curCommit=$(curl --silent https://api.github.com/repos/HoveringHalibut/lflmonitor/git/refs/heads/master | jq -r '.object.sha')
 
 if [ "$curCommit" != "$([ -f ./lastCommit ] && cat ./lastCommit)" ]; then
-  git pull -q
+  git pull -q --recurse-submodules
   echo $curCommit > lastCommit
   # Use separate update script so changes are applied immediately
   ./update.sh
